23fa864a95093d41fcb42503286dffec784e8940
abhiawatade <awatadeabhi@gmail.com>
2022-12-23 18:03:11 +0530

    database created and server connected to database

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 1338;
+const { connectDB } = require("./config/db");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   console.log(`server is listening to the ${port} port`);
+  connectDB();
 });
