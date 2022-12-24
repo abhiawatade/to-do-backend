1332670a58f13c271083654f9e95598403a1f82c
abhiawatade <awatadeabhi@gmail.com>
2022-12-24 10:21:47 +0530

    to do  task database model created

---
+++ models/todoModel.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require("sequelize");
+const { createDB } = require("../config/db");
+
+const Todo = createDB.define("todo", {
+  id: { primaryKey: true, autoIncrement: true, type: DataTypes.INTEGER },
+  title: DataTypes.STRING,
+  description: DataTypes.STRING,
+  dueDate: DataTypes.STRING,
+  completed: DataTypes.BOOLEAN,
+  priority: DataTypes.INTEGER,
+});
+
+module.exports = Todo;
