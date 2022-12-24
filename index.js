71b17049fb83980e85dae9a163bd7b382901d8d1
abhiawatade <awatadeabhi@gmail.com>
2022-12-24 10:22:19 +0530

    to do routes created

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const port = 1338;
 const { connectDB } = require("./config/db");
+const todoRoute = require("./routes/todoRoute");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", todoRoute);
 
 app.listen(port, () => {
   console.log(`server is listening to the ${port} port`);
---
+++ routes/todoRoute.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const Todo = require("../models/todoModel");
+const router = express.Router();
+
+router.get("/todo", async (req, res) => {
+  try {
+    const todos = await Todo.findAll();
+    return res.status(200).json(todos).send("to do get request successfull");
+  } catch (error) {
+    return res.status(404).send(error);
+  }
+});
+
+router.get("/todo/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findOne({ where: { id: req.params.id } });
+    return res.status(200).json(todo).send("to do get request successfull");
+
+    if (!todo) {
+      return res.status(404).send("not found");
+    }
+  } catch (error) {
+    return res.status(404).send(error);
+  }
+});
+
+router.post("/todo", async (req, res) => {
+  try {
+    const { title, description, dueDate, completed, priority } = req.body;
+    if (!title) {
+      return res.status(404).json("Title is missing");
+    }
+    const todo = { title, description, dueDate, completed, priority };
+    const createTodo = await Todo.create(todo);
+    return res.status(200).json({
+      message: `Great your task is added successfully`,
+    });
+  } catch (error) {
+    return res.status(404).send(error);
+  }
+});
+
+router.put("/todo/:id", async (req, res) => {
+  try {
+    const { title, description, dueDate, completed, priority } = req.body;
+    const todo = await Todo.findOne({ where: { id: req.params.id } });
+
+    if (!todo) {
+      return res.status(404).send("To do not found");
+    }
+    if (!title) {
+      return res.status(404).send("title not found");
+    }
+
+    const updateToDo = await Todo.update(
+      { title, description, dueDate, completed, priority },
+      { where: { id: req.params.id } }
+    );
+
+    return res.status(200).json({ message: "todo updated successfully" });
+  } catch (error) {
+    return res.status(404).send(error, "to do doesn't exist ");
+  }
+});
+
+router.delete("/todo/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findOne({ where: { id: req.params.id } });
+
+    if (!todo) {
+      return res.status(404).send("to do task not found");
+    }
+
+    await Todo.destroy({ where: { id: req.params.id } });
+
+    return res.status(200).send("success! todo deleted");
+  } catch (error) {
+    return res.status(404).send(error, "to do doesn't exist ");
+  }
+});
+
+module.exports = router;
